2d2030b9d1a34fe07d029fb5b0112e0446fd5c59
JJ1006 <jaahanavajoshi@gmail.com>
2022-09-11 22:23:04 +0530

    pehla padav paar

---
+++ JJ_codes/main.py
@@ -0,0 +1,15 @@
+import main1
+import main2
+import main3
+import main4
+
+ansimage1 = main1.noofcarsinimage1
+ansimage2 = main2.noofcarsinimage2
+ansimage3 = main3.noofcarsinimage3
+ansimage4 = main4.noofcarsinimage4
+
+print('Number of cars in image 1 is '+ ansimage1)
+print('Number of cars in image 2 is '+ ansimage2)
+print('Number of cars in image 3 is '+ ansimage3)
+print('Number of cars in image 4 is '+ ansimage4)
+
